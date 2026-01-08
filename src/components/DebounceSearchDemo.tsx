1a4269e380b305bb099d199518cb195afe9cefd4
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-08 17:02:23 -0500

    feat(loading state): set isLoading state with useEffect, add loading animation and search results in ternary for conditional UI

--- src/components/DebounceSearchDemo.tsx
+++ src/components/DebounceSearchDemo.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useDebounce from "../customhooks/useDebounce"
 
 export default function DebounceSearchDemo() {
     // use state for controlled serch
-    const [searchInput, setSearchInput] = useState('');
+    const [searchInput, setSearchInput] = useState<string>('');
+    // set up state for loading
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     // handler for search updates on state
     function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
@@ -12,12 +14,22 @@ export default function DebounceSearchDemo() {
     }
 
     // // set up debounce variable with input
-    const debouncedValue = useDebounce(searchInput, 500);
+    const debouncedValue = useDebounce(searchInput, 1500);
+
+    // set a useEffect to listen to both input and debounced value to get a loader
+    useEffect(() => {
+        // if the debounced value hasn't shown up yet, show a loader once the input is being typed and isn't just an empty string
+        if (searchInput !== '' && !debouncedValue) {
+            setIsLoading(true);
+        } else {
+            setIsLoading(false);
+        }
+    }, [searchInput, debouncedValue]);
 
     return (
         <div className="flex flex-col rounded-2xl w-fit h-fit px-10 py-8 bg-green-50 border-2 border-green-200 gap-4">
             <h2 className="text-2xl">Debounce Search Demo</h2>
-            <p className="place-self-start">Debounce Delay (ms): </p>
+            <p className="place-self-start">Debounce Delay (ms): 1500</p>
             {/* input section */}
             <div className="flex flex-col gap-2">
                 <label htmlFor="searchInput">Search</label>
@@ -27,12 +39,20 @@ export default function DebounceSearchDemo() {
             <div className="flex flex-col gap-2 text-left">
                 <p>Current Input: {searchInput} </p>
                 <p>Debounced Input Value: {debouncedValue}</p>
-                <p>Simulated Search Results</p>
-                    <ol>
-                        <li>Result for '{debouncedValue}': item 1</li>
-                        <li>Result for '{debouncedValue}': item 2</li>
-                        <li>Result for '{debouncedValue}': item 3</li>
-                    </ol>
+
+                {/* set up conditional rendering base off if there's a debounced value with a loader before */}
+                <div>
+                    <p>Simulated Search Results</p>
+                    {isLoading ? (
+                        <div className="animate-pulse">Loading...</div>
+                    ) : debouncedValue ? (
+                        <ol>
+                            <li>Result for '{debouncedValue}': item 1</li>
+                            <li>Result for '{debouncedValue}': item 2</li>
+                            <li>Result for '{debouncedValue}': item 3</li>
+                        </ol>
+                    ) : null}
+                </div>              
             </div>
         </div>
     )
