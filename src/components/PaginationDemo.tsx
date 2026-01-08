60f7f58b0251bc1817737c00edf50b41e461a40c
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-08 10:27:55 -0500

    feat(select items): add controlled select element for items per page and base pagination demo structure

--- src/components/PaginationDemo.tsx
+++ src/components/PaginationDemo.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 export default function PaginationDemo() {
+    const [selectItemsPerPage, setSelectItemsPerPage] = useState(10);
+
+    // handle changing select
+    function handleSelect(event: React.ChangeEvent<HTMLSelectElement>) {
+        // set number to the string value
+        const numberChosen = Number(event.target.value)
+        setSelectItemsPerPage(numberChosen)
+    }
+
     return (
-        <>
-            <h2>Pagination Demo</h2>
-        </>
+        <div className="flex flex-col rounded-2xl w-fit h-fit px-10 py-8 bg-blue-50 border-2 border-blue-200">
+            <h2 className="text-2xl">Pagination Demo</h2>
+            <h4 className="text-xl">Total items: </h4>
+            {/* select and total items */}
+            <div className="flex flex-row gap-6">
+                <label htmlFor="itemsPerPage">Items per page:</label>
+                <select id="itemsPerPage" value={selectItemsPerPage} onChange={handleSelect}>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="30">30</option>
+                </select>
+            </div>
+            {/* section with items */}
+            <div>
+
+            </div>
+            {/* page number buttons */}
+            <div>
+
+            </div>
+        </div>
     )
 }
